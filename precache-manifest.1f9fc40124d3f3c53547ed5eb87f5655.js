0814232e890447bff9c53231c35cf912f2c92ef5
Neelam Chaurasia <chaurasia.neelam@gmail.com>
2019-01-17 16:04:52 -0500

    Updates

---
+++ precache-manifest.1f9fc40124d3f3c53547ed5eb87f5655.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9672f063ad36dea2d2565aec8f5b5494",
+    "url": "/weather-app/static/media/weather-app-icon.9672f063.png"
+  },
+  {
+    "revision": "3f46e2d89fc6f238f36c49d5dde4ba56",
+    "url": "/weather-app/static/media/city_vector.3f46e2d8.jpg"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "46f23dd52636d89ff7ce",
+    "url": "/weather-app/static/js/main.46f23dd5.chunk.js"
+  },
+  {
+    "revision": "bf230a832220f7086edc",
+    "url": "/weather-app/static/js/1.bf230a83.chunk.js"
+  },
+  {
+    "revision": "46f23dd52636d89ff7ce",
+    "url": "/weather-app/static/css/main.5d7890e1.chunk.css"
+  },
+  {
+    "revision": "bf230a832220f7086edc",
+    "url": "/weather-app/static/css/1.c7d04f37.chunk.css"
+  },
+  {
+    "revision": "8174a8f1f5b08416d242a33990343542",
+    "url": "/weather-app/index.html"
+  }
+];
